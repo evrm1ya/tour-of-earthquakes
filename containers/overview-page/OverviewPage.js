3e697fb0ab57533cb168730f17c32dd647353ac4
evrm1ya <emvermilyea15@gmail.com>
2016-10-24 19:29:22 -0500

    create layout for overview page

--- containers/overview-page/OverviewPage.js
+++ containers/overview-page/OverviewPage.js
@@ -3,8 +3,23 @@ import React from 'react';
 const OverviewPage = React.createClass({
   render: function() {
     return (
-      <div className='overview-page'>
+      <div className='container-fluid overview-page'>
         <h1>Overview</h1>
+
+        <section className='overview-requester-section'>
+        </section>
+
+        <section className='overview-graphs-section'>
+            <div className='col-md-4'>
+            </div>
+            <div className='col-md-4'>
+            </div>
+            <div className='col-md-4'>
+            </div>
+        </section>
+
+        <section className='overview-table-section'>
+        </section>
       </div>
     );
   }
